529bace0cb35cde0033538aedcfcac10f2ae4c9b
emmatk <emmatk@stud.ntnu.no>
2022-10-20 19:54:46 +0100

    copied line chart

--- app/linechart.js
+++ app/linechart.js
@@ -1,1 +1,67 @@
-var svgContainer = d3.select("body").append("svg");
+const margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select("body")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  }).then(
+
+  // Now I can use this dataset:
+  function(data) {
+
+    // Add X axis --> it is a date format
+    const x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
+
+})
+
+/*
+var dataArray = [2, 13, 15, 22, 9, 17];
+var svg = d3.select("body").append("svg")
+.attr('height', '100%')
+.attr('width', '100%');
+
+var fixedX = 10;
+svg.selectAll('line')
+.data(dataArray)
+.enter().append('line')
+    .attr('x1', fixedX)
+    .attr('y1', function(d,i) {return 400+(i*20)})
+    .attr('x2', function(d,i) {return fixedX+(d*20)})
+    .attr('y2', function(d,i) {return 400+(i*20)});
+svg.append('text').attr('x', '50').attr('y', '250').text('trying to add text');
+*/
