aa5535a125ec9c95ea306f568f86c081afafa184
emmatk <emmatk@stud.ntnu.no>
2022-10-21 16:22:15 +0100

    working

--- app/linechart.js
+++ app/linechart.js
@@ -3,18 +3,24 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// parse the date / time
-
 // set the ranges
 var x = d3.scaleLinear().range([0, width]);  
 var y = d3.scaleLinear().range([height, 0]);
 
-// define the line
-var valueline = d3.line()
+// define the lines
+var popularityLine = d3.line()
     .x(function(d) { return x(d.Year); })
     .y(function(d) { return y(d.Popularity); });
 
-// append the svg obgect to the body of the page
+var acousticnessLine = d3.line()
+    .x(function(d) { return x(d.Year); })
+    .y(function(d) { return y(d.Acousticness); });
+
+var valanceLine = d3.line()
+    .x(function(d) { return x(d.Year); })
+    .y(function(d) { return y(d.Valence); });
+
+// append the svg object to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
 var svg = d3.select("body").append("svg")
@@ -31,27 +37,44 @@ d3.csv("processed-Spotify-2000.csv").then(function(data) {
   data.forEach((d) => {
     if (!dataMap.has(parseInt(d.Year))) {
       var attrList = [];
-      attrList.push(parseInt(d.Popularity));
+      attrList.push({
+        "Popularity": parseInt(d.Popularity), 
+        "Acousticness": parseInt(d.Acousticness), 
+        "Valence": parseInt(d.Valence)
+      });
       dataMap.set(parseInt(d.Year), attrList);
     } else {
-      dataMap.get(parseInt(d.Year)).push(parseInt(d.Popularity));
+      dataMap.get(parseInt(d.Year)).push({
+        "Popularity": parseInt(d.Popularity), 
+        "Acousticness": parseInt(d.Acousticness), 
+        "Valence": parseInt(d.Valence)
+      });
     }
   })
-
+  
   var newMap = new Map();
   for (let y of dataMap.keys()) {
-    let sum = 0;
+    let popSum = 0;
+    let acoSum = 0;
+    let valSum = 0;
     let elements = 0;
-    dataMap.get(y).forEach((val) => {
-      sum+=val;
+    dataMap.get(y).forEach((obj) => {
+      popSum+=obj.Popularity;
+      acoSum+=obj.Acousticness;
+      valSum+=obj.Valence;
       elements ++;
     })
-    newMap.set(y, sum/elements)
+    newMap.set(y, {"Popularity": popSum/elements, "Acousticness": acoSum/elements, "Valence": valSum/elements})
   }
 
   const dataList = [];
   for (let y of dataMap.keys()) {
-    dataList.push({"Year": y, "Popularity": newMap.get(y)});
+    dataList.push({
+      "Year": y, 
+      "Popularity": newMap.get(y).Popularity, 
+      "Acousticness": newMap.get(y).Acousticness, 
+      "Valence": newMap.get(y).Valence
+    });
   }
 
   console.log(dataList);
@@ -63,7 +86,23 @@ d3.csv("processed-Spotify-2000.csv").then(function(data) {
   svg.append("path")
       .data([dataList.sort((a, b) => d3.ascending(a.Year, b.Year))])
       .attr("class", "line")
-      .attr("d", valueline);
+      .attr("d", popularityLine)
+      .attr("stroke", "lightblue")
+      .attr("id", 'popularity'); // assign an ID
+
+  svg.append("path")
+      .data([dataList.sort((a, b) => d3.ascending(a.Year, b.Year))])
+      .attr("class", "line")
+      .attr("d", acousticnessLine)
+      .attr("stroke", "pink")
+      .attr("id", 'acousticness'); // assign an ID
+
+  svg.append("path")
+      .data([dataList.sort((a, b) => d3.ascending(a.Year, b.Year))])
+      .attr("class", "line")
+      .attr("d", valanceLine)
+      .attr("stroke", "orange")
+      .attr("id", 'valance'); // assign an ID
 
   // Add the x Axis
   svg.append("g")
@@ -74,5 +113,5 @@ d3.csv("processed-Spotify-2000.csv").then(function(data) {
   // Add the y Axis
   svg.append("g")
       .call(d3.axisLeft(y));
-
 });
+
