6344f05bfd1de3be21c39501cc92c794489c19bf
emmatk <emmatk@stud.ntnu.no>
2022-10-22 17:41:30 +0100

    cleaning, changes working

--- app/linechart.js
+++ app/linechart.js
@@ -1,9 +1,21 @@
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+const margin = { top: 20, right: 30, bottom: 40, left: 90 };
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
 
-// set the ranges
+function init() {
+  createLineChart("#vi1");
+}
+
+function createLineChart(id) {
+  const svg = d3
+  .select(id)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("id", "gLineChart")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
 var x = d3.scaleLinear().range([0, width]);  
 var y = d3.scaleLinear().range([height, 0]);
 
@@ -23,12 +35,7 @@ var valanceLine = d3.line()
 // append the svg object to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+
 
 // Get the data
 d3.csv("processed-Spotify-2000.csv").then(function(data) {
@@ -115,3 +122,4 @@ d3.csv("processed-Spotify-2000.csv").then(function(data) {
       .call(d3.axisLeft(y));
 });
 
+}
