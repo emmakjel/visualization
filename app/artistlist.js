3304b18a75eed4375474025c6ee87c6fa5e6adb5
emmatk <emmatk@stud.ntnu.no>
2022-11-06 19:35:31 +0000

    word clouds finished??

--- app/artistlist.js
+++ app/artistlist.js
@@ -1,7 +1,8 @@
 const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 const width = 430;
-const height = 430;
+const height = 260;
 const DECADE_INDEX_DICT = { "all": 0, "fifties": 2, "sixties": 4, "seventies": 6, "eighties": 8, "nineties": 10, "twothousands": 12, "twentytens": 14 };
+const WORD_COLORS = {0: "#052350", 1: "#08306b", 2: "#08519c", 3: "#2171b5", 4: "#4292c6", 5: "#6baed6", 6: "#9ecae1", 7: "#c6dbef", 8: "#deebf7", 9: "#f7fbff"};
 
 export function createArtistList(csv, id) {
     var idCropped = id.substring(1);
@@ -11,12 +12,11 @@ export function createArtistList(csv, id) {
               let item = { "name": element.all, "count": parseInt(element.allCount) }
               top_list.push(item);
           });
-        var fill = d3.schemeBlues[9]
-        
+
         d3.layout.cloud().size([width - margin.left - margin.right, height - margin.top - margin.bottom])
         .timeInterval(20)
         .words(top_list)
-        .fontSize(30)
+        .fontSize(18)
         .text(function(d) { return d.name; })
         .rotate(0)
         .font("Impact")
@@ -33,9 +33,12 @@ export function createArtistList(csv, id) {
           .selectAll("text")
           .data(words)
           .enter().append("text")
-          .style("font-size", "30px")
+          .style("font-size", "18px")
           .style("font-family", "Impact")
-          .style("fill", function(d, i) { return fill[i]; })
+          .style("fill", function(d, i) { return WORD_COLORS[i]; })
+          .style("stroke", "#5A6E82")
+          .style("stroke-width", "0.5px")
+          .style("stroke-opacity", 0.5)
           .attr("text-anchor", "middle")
           .attr("transform", function(d) {
             return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
@@ -56,14 +59,14 @@ export function createArtistList(csv, id) {
               let item = { "name": element[Object.keys(element)[DECADE_INDEX_DICT[decade]]], "count": parseInt(element[Object.keys(element)[decade_index + 1]]) }
               top_list.push(item);
           });
+
+          console.log(top_list)
       d3.select("#gWordCloudvi4").remove();
-  
-      var fill = d3.schemeSet3
   
       d3.layout.cloud().size([width - margin.left - margin.right, height - margin.top - margin.bottom])
       .timeInterval(20)
       .words(top_list)
-      .fontSize(30)
+      .fontSize(18)
       .text(function(d) { return d.name; })
       .rotate(0)
       .font("Impact")
@@ -80,9 +83,12 @@ export function createArtistList(csv, id) {
         .selectAll("text")
         .data(words)
         .enter().append("text")
-        .style("font-size", 30)
+        .style("font-size", "18px")
         .style("font-family", "Impact")
-        .style("fill", function(d, i) { return fill[i]; })
+        .style("fill", function(d, i) { return WORD_COLORS[i]; })
+        .style("stroke", "#5A6E82")
+        .style("stroke-opacity", 0.5)
+        .style("stroke-width", "0.5px")
         .attr("text-anchor", "middle")
         .attr("transform", function(d) {
           return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
--- app/wordcloud.js
+++ app/wordcloud.js
@@ -1,25 +1,9 @@
 const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 const width = 430;
-const height = 430;
+const height = 260;
 const DECADE_INDEX_DICT = { "all": 0, "fifties": 2, "sixties": 4, "seventies": 6, "eighties": 8, "nineties": 10, "twothousands": 12, "twentytens": 14 };
-const DECADE_WORD_COUNT_DICT = {"all": 1, "fifties": 40, "sixties": 10, "seventies": 4, "eighties": 4, "nineties": 6, "twothousands": 8, "twentytens": 5}
-const DECADE_ARTIST_COUNT_DICT = {"all": 1, "fifties": 13, "sixties": 2, "seventies": 3, "eighties": 3, "nineties": 3, "twothousands": 4, "twentytens": 2.5}
-
-function sixtiesArtistCount(size) {
-  var result = 0;
-  if (size > 20) {
-    result = 40;
-  } else if (size == 12) {
-    result = 22
-  } else if (size == 7) {
-    result = 16
-  } else if (size == 5) {
-    result = 12
-  } else if (size == 4) {
-    result = 10
-  }
-  return result;
-}
+const DECADE_WORD_COUNT_DICT = {"all": 0.6, "fifties": 35, "sixties": 6, "seventies": 3, "eighties": 3, "nineties": 4, "twothousands": 6, "twentytens": 4}
+const WORD_COLORS = {0: "#052350", 1: "#08306b", 2: "#08519c", 3: "#2171b5", 4: "#4292c6", 5: "#6baed6", 6: "#9ecae1", 7: "#c6dbef", 8: "#deebf7", 9: "#f7fbff"};
 
 
 function createWordCloud(csv, id) {
@@ -32,12 +16,11 @@ function createWordCloud(csv, id) {
             let item = { "name": element.all, "count": parseInt(element.allCount) }
             top_list.push(item);
         });
-      var fill = d3.schemeSet3
 
       d3.layout.cloud().size([width - margin.left - margin.right, height - margin.top - margin.bottom])
       .timeInterval(20)
       .words(top_list)
-      .fontSize(function(d) { return d.count; })
+      .fontSize(function(d) { return d.count * 0.6; })
       .text(function(d) { return d.name; })
       .rotate(function() { return ~~(Math.random() * 2) * 90; })
       .font("Impact")
@@ -54,9 +37,12 @@ function createWordCloud(csv, id) {
         .selectAll("text")
         .data(words)
         .enter().append("text")
-        .style("font-size", function(d) { return d.count + "px"; })
+        .style("font-size", function(d) { return d.count * 0.6 + "px"; })
         .style("font-family", "Impact")
-        .style("fill", function(d, i) { return fill[i]; })
+        .style("stroke", "#5A6E82")
+        .style("stroke-opacity", 0.5)
+        .style("fill", function(d, i) { return WORD_COLORS[i]; })
+        .style("stroke-width", "0.8px")
         .attr("text-anchor", "middle")
         .attr("transform", function(d) {
           return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
@@ -78,7 +64,6 @@ function updateTitleCloud(csv, decade) {
         });
     d3.select("#gWordCloudvi3").remove();
 
-    var fill = d3.schemeSet3
 
     d3.layout.cloud().size([width - margin.left - margin.right, height - margin.top - margin.bottom])
     .timeInterval(20)
@@ -102,7 +87,10 @@ function updateTitleCloud(csv, decade) {
       .enter().append("text")
       .style("font-size", function(d) { return d.count * DECADE_WORD_COUNT_DICT[decade] + "px"; })
       .style("font-family", "Impact")
-      .style("fill", function(d, i) { return fill[i]; })
+      .style("stroke", "#5A6E82")
+      .style("stroke-opacity", 0.5)
+      .style("fill", function(d, i) { return WORD_COLORS[i]; })
+      .style("stroke-width", "0.5px")
       .attr("text-anchor", "middle")
       .attr("transform", function(d) {
         return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
@@ -115,54 +103,8 @@ function updateTitleCloud(csv, decade) {
 }
 
 
-function updateArtistCloud(csv, decade) {
-  var id = "#vi4"
-    d3.csv(csv).then(function (data) {
-        var top_list = [];
-        data.forEach(element => {
-            const decade_index = DECADE_INDEX_DICT[decade];
-            let item = { "name": element[Object.keys(element)[DECADE_INDEX_DICT[decade]]], "count": parseInt(element[Object.keys(element)[decade_index + 1]]) }
-            top_list.push(item);
-        });
-    d3.select("#gWordCloudvi4").remove();
 
-    var fill = d3.schemeSet3
-
-    d3.layout.cloud().size([width - margin.left - margin.right, height - margin.top - margin.bottom])
-    .timeInterval(20)
-    .words(top_list)
-    .fontSize(function(d) { return decade == "sixties" ? sixtiesArtistCount(d.count) : d.count * DECADE_ARTIST_COUNT_DICT[decade]; })
-    .text(function(d) { return d.name; })
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Impact")
-    .on("end", draw)
-    .start();
-
-    function draw(words) {
-      d3.select(id)
-      .attr('width', width - margin.left - margin.right)
-      .attr('height', height - margin.top - margin.bottom)
-      .append("g")
-      .attr("id", "gWordCloudvi4")
-      .attr("transform", "translate(" + [width - margin.left - margin.right >> 1, height - margin.top - margin.bottom >> 1] + ")")
-      .selectAll("text")
-      .data(words)
-      .enter().append("text")
-      .style("font-size", function(d) { return decade == "sixties" ? sixtiesArtistCount(d.count) : d.count * DECADE_ARTIST_COUNT_DICT[decade] + "px"; })
-      .style("font-family", "Impact")
-      .style("fill", function(d, i) { return fill[i]; })
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.name; });
-    }
-    d3.layout.cloud().stop();
-    })
-
-}
-
-export { createWordCloud, updateTitleCloud, updateArtistCloud}
+export { createWordCloud, updateTitleCloud}
 
 
 
